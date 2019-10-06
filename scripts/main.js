e2c2dc7cac7e915ddfadda6e09b255fe9f7333ba
lex8787 <lebedev_a87@mail.ru>
2019-10-06 14:36:07 +0300

    add panorama

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+
+				  
+
+var myHeading = document.querySelector("h1");
+myHeading.textContent = "Best car!";
+
+
+
+
+var myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+   var mySrc = myImage.getAttribute("src");
+   if(mySrc === "images/1dbb03u-960.jpg") {
+       myImage.setAttribute ("src","images/5dbb03u-960.jpg");
+     } else {
+       myImage.setAttribute ("src" ,"images/1dbb03u-960.jpg");
+       }
+ }  
+   
+ var myButton = document.querySelector("button");
+ var myHeading = document.querySelector("h1");
+ 
+function setUserName() {
+
+   var myName = prompt("Please enter your name.");
+   localStorage.setItem("name", myName);
+   myHeading.innerHTML = "Skoda is cool, "  + myName;
+}
+if(!localStorage.getItem("name")) {
+					setUserName();
+		} else {
+				var storedName = localStorage.getItem("name");
+				myHeading.innerHTML  = "Skoda is cool,  "  + storedName;
+				}
+				
+				myButton.onclick = function () {
+				     setUserName();
+				     }
+				     
+				  
+
